a7daafc9bdab2059f88660048ce3656325e5a8d3
Ievgen Braga <evgenybraga@gmail.com>
2016-01-27 18:28:07 +0200

    2016-01-27 EOD

--- JavaLessons/src/main/java/TestLesson01.java
+++ JavaLessons/src/main/java/TestLesson01.java
@@ -4,12 +4,39 @@
 
 import com.io.FileManager;
 
+import java.io.File;
+import java.util.Calendar;
+import java.util.Date;
+
 public class TestLesson01 {
 
     public static void main(String[] args) {
-        System.out.println(FileManager.calculateFiles("C:\\Temp"));
-        System.out.println(FileManager.calculateFiles("C:\\Windows\\System32\\LogFiles\\"));
+        //System.out.println(FileManager.calculateFiles2("C:\\Temp"));
+        //System.out.println(FileManager.calculateFiles2("C:\\Windows\\System32"));
+        //System.out.println(FileManager.calculateFiles("C:\\Windows\\winsxs"));
+        //System.out.println(FileManager.calculateFiles("C:\\Temp"));
+        //System.out.println(FileManager.calculateDirs("C:\\Users\\IBraga"));
+        //System.out.println(FileManager.calculateFiles("C:\\Users\\IBraga\\Documents"));
+        System.out.println(Calendar.getInstance().getTime());
 
-        System.out.println(FileManager.calculateFiles("C:\\Windows\\System32"));
+        //Positive
+        System.out.println(FileManager.copy("C:\\TEMP\\source", "C:\\TEMP\\target"));
+
+        //Copy to non existing directory ()
+        //System.out.println(FileManager.copy("C:\\TEMP\\LOTUS CLIENT 8.5.exe", "C:\\TEMP\\TEMP01\\"));
+
+        //Copy to file in non existing directory
+        //System.out.println(FileManager.copy("C:\\TEMP\\LOTUS CLIENT 8.5.exe", "C:\\TEMP\\TEMP01\\LOTUS CLIENT 8.5.exe"));
+
+        //Target is same directory
+        //System.out.println(FileManager.copy("C:\\TEMP\\LOTUS CLIENT 8.5.exe", "C:\\TEMP"));
+
+        //Source not exist file is same directory
+        //System.out.println(FileManager.copy("C:\\TEMP\\xxx", "C:\\TEMP\\xxx"));
+
+        //Folder with file to another folder
+        //System.out.println(FileManager.copy("C:\\TEMP\\asdfg", "C:\\TEMP\\asdf"));
+
+        System.out.println(Calendar.getInstance().getTime());
     }
 }
--- JavaLessons/src/main/java/com/io/FileManager.java
+++ JavaLessons/src/main/java/com/io/FileManager.java
@@ -1,7 +1,7 @@
 package com.io;
 
 import java.io.*;
-import java.util.Arrays;
+import java.util.*;
 
 public class FileManager {
 
@@ -11,22 +11,8 @@ public class FileManager {
      * @param path
      * @return
      */
+
     public static int calculateFiles(String path) {
-        int count = getFileCount(path);
-        try {
-            for (File dir : getDirs(path)) {
-                count += calculateFiles(dir.getPath());
-            }
-            return count;
-        } catch (NullPointerException ex) {
-            System.out.println("calculateFiles");
-            System.out.println("path = [" + path + "] " + ex.getMessage());
-            return 0;
-        }
-    }
-
-
-    public static int calculateFiles2(String path) {
         int count = 0;
         try {
             File root = new File(path);
@@ -41,27 +27,11 @@ public class FileManager {
             for (File dir : getDirs(path)) {
                 count += calculateFiles(dir.getPath());
             }
+            //return count;
+        } catch (NullPointerException ex) {
+            //return 0;
+        } finally {
             return count;
-        } catch (NullPointerException ex) {
-            return 0;
-        }
-    }
-
-
-    private static int getFileCount(String path) {
-        try {
-            File root = new File(path);
-            FileFilter filter = new FileFilter() {
-                @Override
-                public boolean accept(File pathname) {
-                    return pathname.isFile();
-                }
-            };
-            return root.listFiles(filter).length;
-        } catch (NullPointerException ex) {
-            System.out.println("getFileCount");
-            System.out.println("path = [" + path + "] " + ex.getMessage());
-            return 0;
         }
     }
 
@@ -76,6 +46,30 @@ public class FileManager {
         return root.listFiles(filter);
     }
 
+    private static List<File> getFiles(String path) {
+        List<File> filesList = new ArrayList<File>();
+        try {
+            File root = new File(path);
+            FileFilter filter = new FileFilter() {
+                @Override
+                public boolean accept(File pathname) {
+                    return pathname.isFile();
+                }
+            };
+            for (File currentFile : root.listFiles(filter)) {
+                filesList.add(currentFile);
+            }
+
+            for (File dir : getDirs(path)) {
+                filesList.addAll(getFiles(dir.getPath()));
+            }
+            //return count;
+        } catch (NullPointerException ex) {
+            //return 0;
+        } finally {
+            return filesList;
+        }
+    }
 
     /**
      * принимает путь к папке, возвращает количество папок в папке и всех подпапках по пути
@@ -84,7 +78,24 @@ public class FileManager {
      * @return
      */
     public static int calculateDirs(String path) {
-        return 0;
+        int count = 0;
+        try {
+            File root = new File(path);
+            FileFilter filter = new FileFilter() {
+                @Override
+                public boolean accept(File pathname) {
+                    return pathname.isDirectory();
+                }
+            };
+            count = root.listFiles(filter).length;
+
+            for (File dir : getDirs(path)) {
+                count += calculateDirs(dir.getPath());
+            }
+        } catch (NullPointerException ex) {
+        } finally {
+            return count;
+        }
     }
 
     /**
@@ -95,16 +106,70 @@ public class FileManager {
      * @return
      */
     public static boolean copy(String from, String to) {
-        return false;
-    }
+        boolean status = false;
+        File checkSource = new File(from);
+        File checkTarget = new File(to);
 
-    /**
-     * метод по перемещению папок и файлов
-     *
-     * @param from - путь к файлу или папке
-     * @param to   - путь к папке куда будет производиться перемещение
-     * @return
-     */
+        try {
+            if (checkSource.exists() && checkSource.isDirectory()) {
+                String root = checkSource.getName();
+                List<File> fileList = getFiles(from);
+                Set<File> folderList = new HashSet<File>();
+                for (File file : fileList){
+                    folderList.add(file.getParentFile());
+                }
+
+                for (File folder : folderList ) {
+                    System.out.println(checkTarget);
+
+                }
+                System.out.println(root);
+                System.out.println(fileList);
+                System.out.println(folderList);
+            }
+            return false;
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            return status;
+        }
+/*
+        if (!checkTarget.exists() || checkTarget.isFile()) {
+            checkTarget.mkdirs();
+        }
+*/
+        /*
+        try (BufferedInputStream sourceStream = new BufferedInputStream(new FileInputStream(from));
+             BufferedOutputStream targetStream = new BufferedOutputStream(new FileOutputStream(to + "\\" + checkSource.getName()))) {
+            int chunkSize = 1024;
+            byte[] chunk = new byte[chunkSize];
+            while (sourceStream.available() > 0) {
+                sourceStream.read(chunk, 0, chunkSize);
+                targetStream.write(chunk);
+                chunkSize = chunkSize <= sourceStream.available() ? chunkSize : sourceStream.available();
+                chunk = new byte[chunkSize];
+            }
+            status = true;
+
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            return status;
+
+        }
+        */
+        }
+
+        /**
+         * метод по перемещению папок и файлов
+         *
+         * @param from - путь к файлу или папке
+         * @param to   - путь к папке куда будет производиться перемещение
+         * @return
+         */
+
     public static boolean move(String from, String to) {
         return false;
     }
